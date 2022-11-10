e00d01c74cb43a3153d84760c69381df8302606e
Erik Pukinskis <erik@snowedin.net>
2022-11-09 18:28:47 -0700

    Don't titleize doc paths if they start with a lowercase letter

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -8,6 +8,7 @@ export const nameFromPath = (path: string) => {
 }
 
 export const addSpaces = (name: string) => {
+  if (name[0].toLowerCase() === name[0]) return name
   if (name.startsWith("_")) return name.replace("_", "")
   else return name.replace(/(.)([A-Z])/g, "$1 $2")
 }
