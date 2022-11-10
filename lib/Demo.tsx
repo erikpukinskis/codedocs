91bcc02a6c08d164f9e441d61ed5ed94ad0e7f3b
Erik Pukinskis <erik@snowedin.net>
2022-11-09 18:28:03 -0700

    Throw an error if you try to use a function as Demo children

--- lib/Demo.tsx
+++ lib/Demo.tsx
@@ -14,5 +14,15 @@ function hasChildren(props: DemoProps): props is HasChildren {
   return Boolean((props as HasChildren).children)
 }
 
-export const Demo = (props: DemoProps) =>
-  hasChildren(props) ? <>{props.children}</> : <props.render />
+export const Demo = (props: DemoProps) => {
+  if (hasChildren(props)) {
+    if (typeof props.children === "function") {
+      throw new Error(
+        `Don't pass function children to <Demo>, pass a render function like: <Demo render={() => ... } />`
+      )
+    }
+    return <>{props.children}</>
+  } else {
+    return <props.render />
+  }
+}
