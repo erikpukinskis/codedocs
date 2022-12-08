42d01b2c41a33b3dec58a23c84fc5af97c0ff9d3
Erik Pukinskis <erik@snowedin.net>
2022-12-07 22:46:53 -0700

    Fix macro typings

--- lib/macro.d.ts
+++
@@ -1,7 +0,0 @@
-import * as codedocs from "../"
-
-declare module "codedocs/macro" {
-  export const DocsApp = codedocs.DocsApp
-  export const Doc = codedocs.Doc
-  export const Demo = codedocs.Demo
-}
