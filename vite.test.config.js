f5ee9e00f9f7c9a41204eb4ef48a1adc3ff409e2
Erik Pukinskis <erik@snowedin.net>
2025-12-01 21:35:51 -0500

    Move to happy-dom

--- vite.test.config.js
+++ vite.test.config.js
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: ["./lib/test/vitest-setup.ts"],
   },
 
